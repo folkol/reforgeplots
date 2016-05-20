27fba058921e8096474b88dbf1d9683df6be652a
Matte <mattias4@kth.se>
2016-05-20 21:59:35 +0200

    Split the points.data in 3 lines

--- plot.py
+++ plot.py
@@ -1,8 +1,14 @@
 from numpy import loadtxt
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-xs, yx, zs, dps = loadtxt('data.csv', delimiter=',', skiprows=1, usecols=(0,1,2), unpack=True)
+
+with open('points.dat') as f:
+    data = f.read()
+
+xs, ys = data.split('|')
+
+print xs, ys
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 Axes3D.plot_wireframe(xs, ys, zs, *args, **kwargs)
-
